5eccf77022d339a876b13592585f3e0fb2f19b3f
Jay Ma <doudoujay@qq.com>
2015-02-02 19:38:36 +0800

    Initial Commit

---
+++ beaconBeta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beaconBeta
+//
+//  Created by jay on 15/2/2.
+//  Copyright (c) 2015年 doudoujay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ beaconBetaTests/beaconBetaTests.swift
@@ -0,0 +1,36 @@
+//
+//  beaconBetaTests.swift
+//  beaconBetaTests
+//
+//  Created by jay on 15/2/2.
+//  Copyright (c) 2015年 doudoujay. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class beaconBetaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

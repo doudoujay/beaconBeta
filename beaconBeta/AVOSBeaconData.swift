773df60f3b165d7eab986627f72032d9102dfa80
Jay Ma <doudoujay@qq.com>
2015-02-14 14:52:00 +0800

    整理好的RawData能在本地log出来

---
+++ beaconBeta/AVOSBeaconData.swift
@@ -0,0 +1,21 @@
+//
+//  AVOSBeaconData.swift
+//  beaconBeta
+//
+//  Created by jay on 15/2/13.
+//  Copyright (c) 2015年 doudoujay. All rights reserved.
+//
+
+import UIKit
+
+class AVOSBeaconData: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
